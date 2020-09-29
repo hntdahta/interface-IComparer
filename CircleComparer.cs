9a0a56a5989c016d8aeeb2224b6e45723acb2d0e
Thanh Dat <datmathoingu1100@gmail.com>
2020-09-29 10:10:51 +0700

    Initial commit

---
+++ CircleComparer.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace interface_IComparer
+{
+  public class CircleComparator : IComparer<Circle>
+  {
+    public int Compare(Circle c1, Circle c2)
+    {
+      if (c1.getRadius() > c2.getRadius()) return 1;
+      else if (c1.getRadius() < c2.getRadius()) return -1;
+      else return 0;
+    }
+  }
+}
---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace interface_IComparer
+{
+    class Class1
+    {
+    }
+}
